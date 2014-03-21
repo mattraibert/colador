3c1dd91c04690bdc708d117bdb3aa6b440b2e18f
Matt J Raibert <mattraibert@gmail.com>
2014-03-21 08:16:55 -0400

    upgraded bdd lib version; filled out project info.

--- src/Test.hs
+++ src/Test.hs
@@ -36,33 +36,11 @@ import Site
 
 main :: IO ()
 main = do
-  runSnapTests [consoleReport]
+  let conf = defaultConfig { reportGenerators = [consoleReport]}
+  runSnapTests conf
                (route routes) app $ do
     eventTests
   putStrLn ""
-
-data FormExpectations a = Value a | ErrorPaths [Text] 
-
-assert :: Bool -> SnapTesting App ()
-assert b = equals b (return True)
-
-form :: (Eq a, Show a)
-     => FormExpectations a
-     -> Form Text AppHandler a
-     -> (Map Text Text)
-     -> SnapTesting App ()
-form expected theForm theParams =
-  do r <- eval $ postForm "form" theForm (const $ return lookupParam)
-     case expected of
-       Value a -> equals (snd r) (return $ Just a)
-       ErrorPaths expectedPaths ->
-         do let viewErrorPaths = map (fromPath . fst) $ viewErrors $ fst r
-            assert (all (`elem` viewErrorPaths) expectedPaths)
-  where lookupParam :: Path -> AppHandler [FormInput]
-        lookupParam pth = case M.lookup (fromPath pth) fixedParams of
-                            Nothing -> return []
-                            Just v -> return [TextInput v]
-        fixedParams = M.mapKeys (T.append "form.") theParams
 
 
 eventTests :: SnapTesting App ()
