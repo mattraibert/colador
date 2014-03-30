b93102b98bffc603e75e516c5a062fcd6dfc99a2
Matt J Raibert <mattraibert@gmail.com>
2014-03-30 12:29:26 -0700

    migration crap

--- migrations/1395264053_up_create_event.sql
+++ migrations/1395264053_up_create_event.sql
@@ -2,5 +2,7 @@ CREATE TABLE Event (
     id serial PRIMARY KEY,
     title text not null,
     content text not null,
-    citation text not null
+    citation text not null,
+    start_year integer,
+    end_year integer
 );
