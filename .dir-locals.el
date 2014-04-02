b8f1f1c0095477bfee43bc62016ca6c1f4ae0e78
Matt Raibert <mattraibert@gmail.com>
2014-04-02 20:07:31 +0000

    deleting local file .dir-locals.el

--- .dir-locals.el
+++
@@ -1,4 +0,0 @@
-((haskell-mode
-  (flycheck-ghc-package-databases "/home/mattraibert/src/h/colador/.cabal-sandbox/x86_64-linux-ghc-7.6.3-packages.conf.d/")
-  (flycheck-ghc-no-user-package-database . t)
-  (flycheck-ghc-search-path "/home/mattraibert/src/h/colador/src")))
